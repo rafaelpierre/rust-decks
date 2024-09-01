c05dfb5230b7ff303a0931f67a345f4dbe711767
Rafael Pierre <rafaelpierre@github.com>
2024-09-01 17:15:40 +0200

    deal

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 
 use emojis;
+use rand::{thread_rng, seq::SliceRandom, rngs::OsRng};
 
 #[derive(Debug)]
 struct Deck {
@@ -9,10 +10,10 @@ struct Deck {
 
 impl Deck {
     fn new() -> Self {
-        let hearts = emojis::get("❤️").unwrap();
-        let spades = emojis::get("♠️").unwrap();
-        let diamonds = emojis::get("♦️").unwrap();
-        let clubs = emojis::get("♣️").unwrap();
+        let hearts = emojis::get("😍").unwrap();
+        let spades = emojis::get("🔪").unwrap();
+        let diamonds = emojis::get("💎").unwrap();
+        let clubs = emojis::get("🏏").unwrap();
 
         //List of suits
         let suits = [
@@ -36,18 +37,32 @@ impl Deck {
             }
         }
 
-        //Create a "deck" variable (or "binding", in Rust)
-        //Alternatively, you can use Vec::new()
-        //let deck: Deck = Deck { cards: Vec::new() };
+        //Implicit return
+        Deck { cards }
 
-        let deck: Deck = Deck { cards };
-        return deck;
     }
+
+    fn shuffle(&mut self) {
+        let mut rng = thread_rng();
+        self.cards.shuffle(&mut rng);
+    }
+
+    fn deal(&mut self, num_cards: usize) -> Vec<String> {
+        self.cards.split_off(
+            self.cards.len() - num_cards
+        )
+    }
+
+
 }
 
 fn main() {
 
-    let deck = Deck::new();
+    let mut deck = Deck::new();
 
-    println!("Here's your deck: {:#?}", deck);
+    deck.shuffle();
+    //TODO: add error handling
+    let cards = deck.deal(3);
+
+    println!("Here's your hand: {:#?}", cards);
 }
